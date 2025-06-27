71049cb5d882e2fc643c8be87fd2319427688b5d
Squelch85 <breathken@naver.com>
2025-06-28 03:08:12 +0900

    강화 파괴구간 수정 및 명칭 축약 제거

--- enhance_rates.js
+++ enhance_rates.js
@@ -9,10 +9,10 @@ window.enhanceRates = {
       "5": { "base":1.0, "bless":[0.5, 0.5], "break":false },
       "6": { "base":0.51, "bless":[0.51], "break":true },
       "7": { "base":0.33, "bless":[0.33], "break":true },
-      "8": { "base":0.33, "bless":[0.33], "break":false },
-      "9": { "base":0.1, "bless":[0.1], "break":false },
-      "10": { "base":0.04, "bless":[0.04], "break":false },
-      "11": { "base":0.01, "bless":[0.01], "break":false }
+      "8": { "base":0.33, "bless":[0.33], "break":true },
+      "9": { "base":0.1, "bless":[0.1], "break":true },
+      "10": { "base":0.04, "bless":[0.04], "break":true },
+      "11": { "base":0.01, "bless":[0.01], "break":true }
     },
     "3": {
       "0": { "base":1.0, "bless":[0.34, 0.33, 0.33], "break":false },
@@ -22,11 +22,11 @@ window.enhanceRates = {
       "4": { "base":0.25, "bless":[0.15, 0.1], "break":true },
       "5": { "base":0.18, "bless":[0.18], "break":true },
       "6": { "base":0.16, "bless":[0.16], "break":true },
-      "7": { "base":0.14, "bless":[0.14], "break":false },
-      "8": { "base":0.1, "bless":[0.1], "break":false },
-      "9": { "base":0.07, "bless":[0.07], "break":false },
-      "10": { "base":0.03, "bless":[0.03], "break":false },
-      "11": { "base":0.01, "bless":[0.01], "break":false }
+      "7": { "base":0.14, "bless":[0.14], "break":true },
+      "8": { "base":0.1, "bless":[0.1], "break":true },
+      "9": { "base":0.07, "bless":[0.07], "break":true },
+      "10": { "base":0.03, "bless":[0.03], "break":true },
+      "11": { "base":0.01, "bless":[0.01], "break":true }
     },
     "0": {
       "0": { "base":0.51, "bless":[0.2, 0.2, 0.11], "break":true },
@@ -35,12 +35,12 @@ window.enhanceRates = {
       "3": { "base":0.33, "bless":[0.165, 0.165], "break":true },
       "4": { "base":0.25, "bless":[0.125, 0.125], "break":true },
       "5": { "base":0.2, "bless":[0.1, 0.1], "break":true },
-      "6": { "base":0.2, "bless":[0.2], "break":false },
-      "7": { "base":0.2, "bless":[0.2], "break":false },
-      "8": { "base":0.15, "bless":[0.15], "break":false },
-      "9": { "base":0.15, "bless":[0.15], "break":false },
-      "10": { "base":0.1, "bless":[0.1], "break":false },
-      "11": { "base":0.1, "bless":[0.1], "break":false }
+      "6": { "base":0.2, "bless":[0.2], "break":true },
+      "7": { "base":0.2, "bless":[0.2], "break":true },
+      "8": { "base":0.15, "bless":[0.15], "break":true },
+      "9": { "base":0.15, "bless":[0.15], "break":true },
+      "10": { "base":0.1, "bless":[0.1], "break":true },
+      "11": { "base":0.1, "bless":[0.1], "break":true }
     }
   },
   "방어구": {
@@ -53,10 +53,10 @@ window.enhanceRates = {
       "5": { "base":1.0, "bless":[0.5, 0.5], "break":false },
       "6": { "base":0.18, "bless":[0.18], "break":true },
       "7": { "base":0.16, "bless":[0.16], "break":true },
-      "8": { "base":0.14, "bless":[0.14], "break":false },
-      "9": { "base":0.07, "bless":[0.07], "break":false },
-      "10": { "base":0.03, "bless":[0.03], "break":false },
-      "11": { "base":0.01, "bless":[0.01], "break":false }
+      "8": { "base":0.14, "bless":[0.14], "break":true },
+      "9": { "base":0.07, "bless":[0.07], "break":true },
+      "10": { "base":0.03, "bless":[0.03], "break":true },
+      "11": { "base":0.01, "bless":[0.01], "break":true }
     },
     "4": {
       "0": { "base":1.0, "bless":[0.34, 0.33, 0.33], "break":false },
@@ -67,10 +67,10 @@ window.enhanceRates = {
       "5": { "base":0.25, "bless":[0.15, 0.1], "break":true },
       "6": { "base":0.18, "bless":[0.18], "break":true },
       "7": { "base":0.16, "bless":[0.16], "break":true },
-      "8": { "base":0.14, "bless":[0.14], "break":false },
-      "9": { "base":0.07, "bless":[0.07], "break":false },
-      "10": { "base":0.03, "bless":[0.03], "break":false },
-      "11": { "base":0.01, "bless":[0.01], "break":false }
+      "8": { "base":0.14, "bless":[0.14], "break":true },
+      "9": { "base":0.07, "bless":[0.07], "break":true },
+      "10": { "base":0.03, "bless":[0.03], "break":true },
+      "11": { "base":0.01, "bless":[0.01], "break":true }
     },
     "3": {
       "0": { "base":1.0, "bless":[0.34, 0.33, 0.33], "break":false },
@@ -80,25 +80,25 @@ window.enhanceRates = {
       "4": { "base":0.25, "bless":[0.15, 0.1], "break":true },
       "5": { "base":0.18, "bless":[0.18], "break":true },
       "6": { "base":0.16, "bless":[0.16], "break":true },
-      "7": { "base":0.14, "bless":[0.14], "break":false },
-      "8": { "base":0.1, "bless":[0.1], "break":false },
-      "9": { "base":0.07, "bless":[0.07], "break":false },
-      "10": { "base":0.03, "bless":[0.03], "break":false },
-      "11": { "base":0.01, "bless":[0.01], "break":false }
+      "7": { "base":0.14, "bless":[0.14], "break":true },
+      "8": { "base":0.1, "bless":[0.1], "break":true },
+      "9": { "base":0.07, "bless":[0.07], "break":true },
+      "10": { "base":0.03, "bless":[0.03], "break":true },
+      "11": { "base":0.01, "bless":[0.01], "break":true }
     },
     "1": {
-      "0": { "base":1.0, "bless":[0.33340000000000003, 0.3333, 0.3333], "break":false },
+      "0": { "base":1.0, "bless":[0.33340000000000003, 0.3333, 0.3333], "break":true },
       "1": { "base":0.7501, "bless":[0.2501, 0.25, 0.25], "break":true },
       "2": { "base":0.49, "bless":[0.1634, 0.16329999999999997, 0.16329999999999997], "break":true },
       "3": { "base":0.33, "bless":[0.165, 0.165], "break":true },
       "4": { "base":0.25, "bless":[0.125, 0.125], "break":true },
       "5": { "base":0.2, "bless":[0.1, 0.1], "break":true },
-      "6": { "base":0.2, "bless":[0.2], "break":false },
-      "7": { "base":0.15, "bless":[0.15], "break":false },
-      "8": { "base":0.15, "bless":[0.15], "break":false },
-      "9": { "base":0.1, "bless":[0.1], "break":false },
-      "10": { "base":0.1, "bless":[0.1], "break":false },
-      "11": { "base":0.05, "bless":[0.05], "break":false }
+      "6": { "base":0.2, "bless":[0.2], "break":true },
+      "7": { "base":0.15, "bless":[0.15], "break":true },
+      "8": { "base":0.15, "bless":[0.15], "break":true },
+      "9": { "base":0.1, "bless":[0.1], "break":true },
+      "10": { "base":0.1, "bless":[0.1], "break":true },
+      "11": { "base":0.05, "bless":[0.05], "break":true }
     },
     "0": {
       "0": { "base":0.5001, "bless":[0.16670000000000001, 0.16670000000000001, 0.16670000000000001], "break":true },
@@ -107,12 +107,12 @@ window.enhanceRates = {
       "3": { "base":0.2, "bless":[0.1, 0.1], "break":true },
       "4": { "base":0.18, "bless":[0.09, 0.09], "break":true },
       "5": { "base":0.16, "bless":[0.08, 0.08], "break":true },
-      "6": { "base":0.14, "bless":[0.14], "break":false },
-      "7": { "base":0.1, "bless":[0.1], "break":false },
-      "8": { "base":0.1, "bless":[0.1], "break":false },
-      "9": { "base":0.1, "bless":[0.1], "break":false },
-      "10": { "base":0.1, "bless":[0.1], "break":false },
-      "11": { "base":0.1, "bless":[0.1], "break":false }
+      "6": { "base":0.14, "bless":[0.14], "break":true },
+      "7": { "base":0.1, "bless":[0.1], "break":true },
+      "8": { "base":0.1, "bless":[0.1], "break":true },
+      "9": { "base":0.1, "bless":[0.1], "break":true },
+      "10": { "base":0.1, "bless":[0.1], "break":true },
+      "11": { "base":0.1, "bless":[0.1], "break":true }
     }
   },
   "그로아 장비": {
@@ -124,11 +124,11 @@ window.enhanceRates = {
       "4": { "base":0.25, "bless":[0.15, 0.1], "break":true },
       "5": { "base":0.18, "bless":[0.18], "break":true },
       "6": { "base":0.16, "bless":[0.16], "break":true },
-      "7": { "base":0.14, "bless":[0.14], "break":false },
-      "8": { "base":0.1, "bless":[0.1], "break":false },
-      "9": { "base":0.07, "bless":[0.07], "break":false },
-      "10": { "base":0.03, "bless":[0.03], "break":false },
-      "11": { "base":0.01, "bless":[0.01], "break":false }
+      "7": { "base":0.14, "bless":[0.14], "break":true },
+      "8": { "base":0.1, "bless":[0.1], "break":true },
+      "9": { "base":0.07, "bless":[0.07], "break":true },
+      "10": { "base":0.03, "bless":[0.03], "break":true },
+      "11": { "base":0.01, "bless":[0.01], "break":true }
     }
   },
   "장신구": {
@@ -140,11 +140,11 @@ window.enhanceRates = {
       "4": { "base":0.25, "bless":[0.15, 0.1], "break":true },
       "5": { "base":0.18, "bless":[0.18], "break":true },
       "6": { "base":0.16, "bless":[0.16], "break":true },
-      "7": { "base":0.14, "bless":[0.14], "break":false },
-      "8": { "base":0.1, "bless":[0.1], "break":false },
-      "9": { "base":0.07, "bless":[0.07], "break":false },
-      "10": { "base":0.03, "bless":[0.03], "break":false },
-      "11": { "base":0.01, "bless":[0.01], "break":false }
+      "7": { "base":0.14, "bless":[0.14], "break":true },
+      "8": { "base":0.1, "bless":[0.1], "break":true },
+      "9": { "base":0.07, "bless":[0.07], "break":true },
+      "10": { "base":0.03, "bless":[0.03], "break":true },
+      "11": { "base":0.01, "bless":[0.01], "break":true }
     },
     "0": {
       "0": { "base":1.0, "bless":[0.33340000000000003, 0.3333, 0.3333], "break":false },
@@ -153,12 +153,12 @@ window.enhanceRates = {
       "3": { "base":0.33, "bless":[0.165, 0.165], "break":true },
       "4": { "base":0.25, "bless":[0.125, 0.125], "break":true },
       "5": { "base":0.2, "bless":[0.1, 0.1], "break":true },
-      "6": { "base":0.2, "bless":[0.2], "break":false },
-      "7": { "base":0.15, "bless":[0.15], "break":false },
-      "8": { "base":0.15, "bless":[0.15], "break":false },
-      "9": { "base":0.1, "bless":[0.1], "break":false },
-      "10": { "base":0.1, "bless":[0.1], "break":false },
-      "11": { "base":0.05, "bless":[0.05], "break":false }
+      "6": { "base":0.2, "bless":[0.2], "break":true },
+      "7": { "base":0.15, "bless":[0.15], "break":true },
+      "8": { "base":0.15, "bless":[0.15], "break":true },
+      "9": { "base":0.1, "bless":[0.1], "break":true },
+      "10": { "base":0.1, "bless":[0.1], "break":true },
+      "11": { "base":0.05, "bless":[0.05], "break":true }
     }
   },
   "컬렉션 전용 아이템": {
@@ -169,12 +169,12 @@ window.enhanceRates = {
       "3": { "base":0.33, "bless":[0.165, 0.165], "break":true },
       "4": { "base":0.25, "bless":[0.125, 0.125], "break":true },
       "5": { "base":0.2, "bless":[0.1, 0.1], "break":true },
-      "6": { "base":0.2, "bless":[0.2], "break":false },
-      "7": { "base":0.15, "bless":[0.15], "break":false },
-      "8": { "base":0.15, "bless":[0.15], "break":false },
-      "9": { "base":0.1, "bless":[0.1], "break":false },
-      "10": { "base":0.1, "bless":[0.1], "break":false },
-      "11": { "base":0.05, "bless":[0.05], "break":false }
+      "6": { "base":0.2, "bless":[0.2], "break":true },
+      "7": { "base":0.15, "bless":[0.15], "break":true },
+      "8": { "base":0.15, "bless":[0.15], "break":true },
+      "9": { "base":0.1, "bless":[0.1], "break":true },
+      "10": { "base":0.1, "bless":[0.1], "break":true },
+      "11": { "base":0.05, "bless":[0.05], "break":true }
     }
   }
 };
